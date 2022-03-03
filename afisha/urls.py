6e82482618de0cce3fd81e86d3dfd12d71cfe20d
Argen <argen1854@gmail.com>
2022-03-03 10:58:59 +0300

    a

--- afisha/urls.py
+++ afisha/urls.py
@@ -4,13 +4,13 @@ from movie_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/directors/', views.DirectorListView),
-    path('api/v1/directors/<int:id>/', views.DirectorDetailView),
-    path('api/v1/movies/', views.MovieListView),
-    path('api/v1/movies/<int:id>/', views.MovieDetailView),
-    path('api/v1/reviews/', views.ReviewListView),
-    path('api/v1/reviews/<int:id>/', views.ReviewDetailView),
-    path('api/v1/movies/reviews/', views.MoviesReviesList),
-    path('api/v1/authorization', views.authorization),
-    path('api/v1/registration', views.registration),
+    path('api/v1/directors/', views.DirectorListAPIView.as_view()),
+    path('api/v1/directors/<int:id>/', views.DirectorDetailAPIView.as_view()),
+    path('api/v1/movies/', views.MovieListAPIView.as_view()),
+    path('api/v1/movies/<int:id>/', views.MovieDetailAPIView.as_view()),
+    path('api/v1/reviews/', views.ReviewListAPIView.as_view()),
+    path('api/v1/reviews/<int:id>/', views.ReviewDetailView.as_view()),
+    path('api/v1/movies/reviews/', views.MovieReviesListAPIView.as_view()),
+    path('api/v1/authorization/', views.Authorization.as_view()),
+    path('api/v1/registration/', views.Registration.as_view()),
 ]
